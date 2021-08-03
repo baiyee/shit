ec3f7934d07358d73a0b8ebcb34c5445829e0a37
张宇璇 <zhangxuan@do1.com.cn>
2021-08-03 12:09:52 +0800

    init project

---
+++ src/main/java/com/zx/shit/ShitApplication.java
@@ -0,0 +1,13 @@
+package com.zx.shit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zx/shit/ShitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zx.shit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
